ddc2565fbf13ab0657b3e16793f2ed70a34b7516
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2016-11-24 09:46:52 +0700

    Manage content

--- resources/views/controllers/words/partials/ad-billboard.blade.php
+++ resources/views/controllers/words/partials/ad-billboard.blade.php
@@ -1,1 +1,3 @@
-
+@if (File::exists($path = storage_path('ad/billboard.txt')))
+	{!! File::get($path) !!}
+@endif
--- resources/views/partials/ga-analytic.blade.php
+++ resources/views/partials/ga-analytic.blade.php
@@ -1,1 +1,3 @@
-
+@if (File::exists($path = storage_path('tool/ga-analytic.txt')))
+	{!! File::get($path) !!}
+@endif
