316e0b505c1c507e5f660ea52494df481df8c9fb
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2016-11-24 12:18:31 +0700

    Remove ad from 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,10 +4,6 @@
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            @include('controllers.words.partials.ad-billboard')
-        </div>
-
-        <div class="col-md-12">
             <div class="panel panel-primary">
                 <div class="panel-heading">Halaman tidak ditemukan</div>
                 <div class="panel-body text-center">
