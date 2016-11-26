8365026ccf04c0b91de1a7cfe16c47f2c4c4dbfe
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2016-11-26 14:44:00 +0700

    Add space between ad

--- resources/views/controllers/words/api.blade.php
+++ resources/views/controllers/words/api.blade.php
@@ -17,7 +17,7 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-12">
+        <div class="col-md-12" style="margin-bottom: 20px;">
             @include('partials.ad-billboard')
         </div>
 
