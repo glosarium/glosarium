aa2bbfb58161fb3428e49cc71242a1e0a0cc8340
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2017-01-11 12:32:48 +0700

    Modify dictionary lib

--- app/Library/Dictionary.php
+++ app/Library/Dictionary.php
@@ -3,6 +3,7 @@
 namespace App\Library;
 
 use App\Glosarium\Word;
+use App\Glosarium\WordType;
 use App\Glosarium\WordDescription;
 
 /**
@@ -42,6 +43,19 @@ class Dictionary
     public function __construct(Word $word)
     {
         $this->word = $word;
+
+        if ($this->word->retry <= config('word.tries')) {
+            // get remote content
+            $client = new \Goutte\Client;
+
+            $entry = !empty($this->word->alias) ? urlencode($this->word->alias) : urlencode(strtolower($this->word->locale));
+
+            $this->content = $client->request('GET', $this->url . $entry);
+
+            if ($this->content->filter('div.kbbi4 > ol')->count() == 0) {
+                \Log::debug('Word not found for: ' . $entry);
+            }
+        }
     }
 
     /**
@@ -74,200 +88,72 @@ class Dictionary
     }
 
     /**
-     * Get live information from KBBI
-     *
-     * @author Yugo <dedy.yugo.purwanto@gmail.com>
-     * @return void
-     */
-    public function getRemoteContent()
-    {
-        // find description in KBBI
-        $client = new \Goutte\Client;
-
-        $entry = !empty($this->word->alias) ? urlencode($this->word->alias) : urlencode(strtolower($this->word->locale));
-
-        $this->content = $client->request(
-            'GET',
-            $url = $this->url . $entry
-        );
-
-        if ($this->content->filter('div.kbbi4 > ol')->count() == 0) {
-            \Log::debug('Word not found for: ' . $entry);
-        }
-
-        return $this;
-    }
-
-    /**
      * Parse DOM to get spell word
      *
      * @author Yugo <dedy.yugo.purwanto@gmail.com>
      * @return object $this
      */
-    public function getSpell(): string
+    public function spell(): string
     {
-        abort_if(empty($this->content), 500, 'Empty response content from dictionary.');
-
-        if (empty($this->word->spell)) {
-            $element = 'h2 > span.syllable';
-
-            if ($this->content->filter($element)->count() > 0) {
-                $this->word->spell = $this->content->filter($element)->first()->text();
-                $this->word->save();
-            }
-        } else {
-            \Log::info('Spell not found for: ' . $this->word->locale);
+        // no remote content and get if from database
+        if (! empty($this->word->spell) OR empty($this->content)) {
+            return $this->word->spell;
         }
 
-        if (is_null($this->word->spell)) {
-            return '';
+        $element = $this->content->filter('h2 > span.syllable');
+
+        if ($element->count() > 0) {
+            return $element->first()->text();
         }
 
-        return $this->word->spell;
+        return '';
     }
 
-    /**
-     * Parse DOM to get descriptions
-     *
-     * @author Yugo <dedy.yugo.purwanto@gmail.com>
-     * @return object $this
-     */
-    public function getDescriptions(): array
+    public function descriptions()
     {
-        abort_if(empty($this->content), 500, 'Empty response content from dictionary.');
-
         $element = $this->content->filter('ol > li');
-        if ($element->count() <= 0) {
-            return [];
+
+        $types = WordType::where('alias', '!=', '')->pluck('id', 'alias');
+
+        if ($element->count() >= 1) {
+            $descriptions = $element->each(function($node, $i) use($types) {
+                list($type, $text) = explode(' ', $node->text(), 2);
+
+                return [
+                    'id' => array_key_exists($type, $types->toArray()) ? $types->get($type) : 0,
+                    'type' => $type,
+                    'text' => trim($node->text())
+                ];
+            });
+
+            return $descriptions;
         }
 
-        $first = $element->first()->text();
-
-        $word = $this->word;
-
-        if ($this->word->descriptions->count() <= 0) {
-            if (strpos(trim($first), ' ') !== false) {
-
-                $descriptions = $element->each(function ($li, $i) use ($word) {
-
-                    if (strpos(trim($li->text()), ' ') !== false) {
-                        list($type, $description) = explode(' ', $li->text(), 2);
-                    } else {
-                        $type = null;
-                        $description = $li->text();
-                    }
-
-                    // static data
-                    $classAlias = [
-                        null   => 0,
-                        'n'    => 2,
-                        'v'    => 1,
-                        'a'    => 5,
-                        'adv'  => 6,
-                        'num'  => 4,
-                        'p'    => 7,
-                        'pron' => 3,
-                    ];
-
-                    return [
-                        'word_id'     => $this->word->id,
-                        'type_id'     => isset($classAlias[$type]) ? $classAlias[$type] : 0,
-                        'description' => empty($type) ? $description : sprintf('%s: %s', $type, $description),
-                        'created_at'  => \Carbon\Carbon::now(),
-                        'updated_at'  => \Carbon\Carbon::now(),
-                    ];
-                });
-            } else {
-                $client = new \Goutte\Client;
-                $alias = $client->request('GET', $this->url . $first);
-
-                // update word alias
-                list($aliasWord, $spell) = explode('/', $alias->filter('h2')->text(), 2);
-
-                $this->word->alias = trim($first);
-                $this->word->locale = trim(ucwords($aliasWord));
-                $this->word->save();
-
-                $aliasElement = $alias->filter('ol > li');
-                $descriptions = $aliasElement->each(function ($li, $i) use ($word) {
-
-                    list($type, $description) = explode(' ', $li->text(), 2);
-
-                    // static data
-                    $classAlias = [
-                        'n'    => 2,
-                        'v'    => 1,
-                        'a'    => 5,
-                        'adv'  => 6,
-                        'num'  => 4,
-                        'p'    => 7,
-                        'pron' => 3,
-                    ];
-
-                    return [
-                        'word_id'     => $this->word->id,
-                        'type_id'     => isset($classAlias[$type]) ? $classAlias[$type] : 0,
-                        'description' => empty($type) ? $description : sprintf('%s: %s', $type, $description),
-                        'created_at'  => \Carbon\Carbon::now(),
-                        'updated_at'  => \Carbon\Carbon::now(),
-                    ];
-                });
-            }
-
-            WordDescription::insert($descriptions);
-        } else {
-            \Log::debug('Word descriptions not found for:' . $this->word->locale);
-        }
-
-        if (is_null($this->word->descriptions)) {
-            return [];
-        }
-
-        return $this->word->descriptions->toArray();
+        return [];
     }
 
-    /**
-     * Get word instance as "Kata turunan"
-     *
-     * @author Yugo <dedy.yugo.purwanto@gmail.com>
-     * @return string
-     */
-    public function getWordInstance(): string
+    public function __destruct()
     {
-        if (!empty($this->content)) {
-            $element = $this->content->filter('dl.turunan > dd');
-            if ($element->count() >= 0) {
-                $instance = $element->first()->each(function ($dd, $i) {
-                    return str_replace("\n", null, $dd->text());
-                });
+        if ($this->word->retry <= config('word.tries')) {
+            $this->word->spell = $this->spell();
+            $this->word->increment('retry', 1);
+            $this->word->save();
 
-                return end($instance);
+            if ($this->word->descriptions->count() <= 0) {
+                $descriptions = [];
+                foreach ($this->descriptions() as $description) {
+                    $descriptions[] = [
+                        'word_id' => $this->word->id,
+                        'type_id' => $description['id'],
+                        'description' => $description['text'],
+                        'created_at' => \Carbon\Carbon::now(),
+                        'updated_at' => \Carbon\Carbon::now(),
+                    ];
+                }
+
+                WordDescription::insert($descriptions);
             }
         }
-
-        return '';
-    }
-
-    /**
-     * Get word join as "Kata gabungan"
-     *
-     * @author Yugo <dedy.yugo.purwanto@gmail.com>
-     * @return string
-     */
-    public function getWordJoin(): string
-    {
-        if (!empty($this->content)) {
-            $element = $this->content->filter('dl.turunan > dd');
-            if ($element->count() >= 0) {
-                $joins = $element->eq(1)->each(function ($dd, $i) {
-                    return str_replace("\n", null, $dd->text());
-                });
-
-                return end($joins);
-            }
-        }
-
-        return '';
     }
 
 }
