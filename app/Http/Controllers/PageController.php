a0863a122d295387c743d792203d8a8a79555383
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2017-01-17 12:40:46 +0700

    Store total into cache

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -1,24 +1,47 @@
 <?php
 
+/**
+ * Glosarium adalah aplikasi berbasis web yang menyediakan berbagai kata glosarium,
+ * kamus nasional dan kamus bahasa daerah.
+ *
+ * @author Yugo <dedy.yugo.purwanto@gmail.com>
+ * @copyright Glosarium - 2017
+ */
+
 namespace App\Http\Controllers;
 
+use Cache;
+
+/**
+ * Default homepage
+ */
 class PageController extends Controller
 {
     public function index()
     {
-        $total['glosarium'] = \App\Glosarium\Word::count();
+        $cacheTime = \Carbon\Carbon::now()->addDays(7);
 
-        $total['dictionary'] = \App\Dictionary\Word::count();
+        $total['glosarium'] = Cache::remember('glosarium.total', $cacheTime, function () {
+            return \App\Glosarium\Word::count();
+        });
 
-        $total['category'] = \App\Glosarium\Category::count();
+        $total['dictionary'] = Cache::remember('dictionary.total', $cacheTime, function () {
+            return \App\Dictionary\Word::count();
+        });
 
-        $total['user'] = \App\User::count();
+        $total['category'] = Cache::remember('category.total', $cacheTime, function () {
+            return \App\Glosarium\Category::count();
+        });
+
+        $total['user'] = Cache::remember('user.total', $cacheTime, function () {
+            return \App\User::count();
+        });
 
         return view('pages.index', compact('total'));
     }
 
     public function show($page)
     {
-        // code...
+
     }
 }
