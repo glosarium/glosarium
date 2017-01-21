67f439efb0e3188a67d2a136c5aad7f0d93d83e5
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2017-01-21 20:25:32 +0700

    Send reset password link via email

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Http\Request;
 
 class ForgotPasswordController extends Controller
 {
@@ -36,5 +37,36 @@ class ForgotPasswordController extends Controller
     public function showLinkRequestForm()
     {
         return view('controllers.auth.passwords.email');
+    }
+
+    /**
+     * Send a reset link to the given user.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function sendResetLinkEmail(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|email|exists:users,email',
+        ]);
+
+        // We will send the password reset link to this user. Once we have attempted
+        // to send the link, we will examine the response then see the message we
+        // need to show to the user. Finally, we'll send out a proper response.
+        $response = $this->broker()->sendResetLink(
+            $request->only('email')
+        );
+
+        if (request()->ajax()) {
+            return response()->json([
+                'isSuccess' => true,
+                'message'   => 'Surel instruksi berhasil dikirim ke kotak masuk.',
+            ]);
+        }
+
+        return $response == Password::RESET_LINK_SENT
+        ? $this->sendResetLinkResponse($response)
+        : $this->sendResetLinkFailedResponse($request, $response);
     }
 }
--- resources/views/auths/logins/form.blade.php
+++ resources/views/auths/logins/form.blade.php
@@ -17,11 +17,11 @@
 
                     <div class="form-group {{ $errors->has('email') ? 'has-error' : '' }}">
                         <label>Alamat Surel</label>
-                        <input name="email" type="email" class="form-control" placeholder="">
+                        <input v-model="forms.email" name="email" type="email" class="form-control" placeholder="">
                     </div>
                     <div class="form-group {{ $errors->has('password') ? 'has-error' : '' }}">
                         <label>Katasandi</label>
-                        <input name="password" type="password" class="form-control" placeholder="">
+                        <input v-model="forms.password" name="password" type="password" class="form-control" placeholder="">
                     </div>
                     <div class="form-group">
                         <div class="row">
@@ -35,7 +35,7 @@
                                 </div>
                             </div>
                             <div class="col-xs-6 text-right">
-                                <p class="help-block"><a href="#myModal" data-toggle="modal">Lupa katasandi?</a></p>
+                                <p class="help-block"><a href="#password-modal" data-toggle="modal">Lupa katasandi?</a></p>
                             </div>
                         </div>
                     </div>
@@ -50,12 +50,96 @@
 </div>
 <div class="white-space-20"></div>
 <div class="text-center color-white">Bukan kontributor? &nbsp; <a href="{{ url('register') }}" class="link-white"><strong>Buat akun gratis!</strong></a></div>
+
+<div class="modal fade" id="password-modal" >
+    <div class="modal-dialog">
+        <div class="modal-content">
+
+            <form v-on:submit.prevent="sendEmail" action="{{ url('password/email') }}" method="post" role="form">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title" >Lupa Katasandi?</h4>
+                </div>
+                <div class="modal-body">
+
+                    <div v-if="passwordAlerts.message" class="alert alert-success">
+                        @{{ passwordAlerts.message }}
+                    </div>
+
+                    <div v-bind:class="['form-group', errors && errors.emailForgot ? 'has-error' : '']">
+                        <label>Masukkan Alamat Surel</label>
+                        <input v-model="forms.emailForgot" type="email" class="form-control" name="email" autocomplete="off" v-bind:disabled="isDisabled">
+                        <span v-if="errors && errors.emailForgot" class="label label-danger">@{{ errors.emailForgot[0] }}</span>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <i v-if="loading" class="fa fa-spinner fa-spin"></i>
+                    <button v-bind:disabled="isDisabled" type="button" class="btn btn-default btn-theme" data-dismiss="modal">Tutup</button>
+                    <button v-bind:disabled="isDisabled" type="submit" class="btn btn-success btn-theme">Kirim</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<!-- end modal forgot password -->
+
+
 @endsection
 
 @push('js')
     <script>
         $(function(){
             $('#content').addClass('block-section bg-color4');
+        });
+
+        var login = new Vue({
+            el: '#content',
+            data: {
+                loading: false,
+                isDisabled: false,
+                forms: {
+                    _token: Laravel.csrfToken,
+                    email: null,
+                    password: null,
+                    emailForgot: null
+                },
+                errors: {
+                    emailForgot: null
+                },
+                passwordAlerts: {
+                    message: null
+                }
+            },
+
+            methods: {
+
+                sendEmail: function(e) {
+                    var forms = {
+                        _token: this.forms._token,
+                        email: this.forms.emailForgot
+                    };
+
+                    this.loading = true;
+                    this.errors = null;
+                    this.passwordAlerts = {
+                        message: null
+                    };
+                    this.isDisabled = true;
+
+                    this.$http.post(e.target.action, forms).then(function(response){
+                        this.passwordAlerts.message = response.body.message;
+
+                        this.loading = false;
+                        this.isDisabled = false;
+                        this.forms.emailForgot = null;
+                    }, function(response){
+                        this.errors.emailForgot = response.body.email;
+
+                        this.loading = false;
+                        this.isDisabled = false;
+                    })
+                }
+            }
         })
     </script>
 @endpush
