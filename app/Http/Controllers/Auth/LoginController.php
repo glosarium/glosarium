67213996d97f747ba1750557f16d12cef55ff03c
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2017-01-21 20:49:12 +0700

    Login via ajax

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Libraries\Image;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -55,5 +56,57 @@ class LoginController extends Controller
 
         return view('auths.logins.form', compact('imagePath'))
             ->withTitle($title);
+    }
+
+    /**
+     * Handle a login request to the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+
+        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+        // the login attempts for this application. We'll key this by the username and
+        // the IP address of the client making these requests into this application.
+        if ($this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
+        if ($this->attemptLogin($request)) {
+            if (request()->ajax()) {
+                return response()->json([
+                    'isSuccess' => true,
+                    'url'       => route('index'),
+                ]);
+            }
+
+            return $this->sendLoginResponse($request);
+        }
+
+        // If the login attempt was unsuccessful we will increment the number of attempts
+        // to login and redirect the user back to the login form. Of course, when this
+        // user surpasses their maximum number of attempts they will get locked out.
+        $this->incrementLoginAttempts($request);
+
+        return $this->sendFailedLoginResponse($request);
+    }
+
+    /**
+     * Validate the user login request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return void
+     */
+    protected function validateLogin(Request $request)
+    {
+        $this->validate($request, [
+            $this->username() => 'required|exists:users,email',
+            'password'        => 'required',
+        ]);
     }
 }
--- resources/views/auths/logins/form.blade.php
+++ resources/views/auths/logins/form.blade.php
@@ -6,29 +6,31 @@
         <div class="row">
             <div class="col-md-12">
                 <!-- buttons top -->
-                <p><a href="#" class="btn btn-primary btn-theme btn-block"><i class="fa fa-facebook pull-left bordered-right"></i> Login with Facebook</a></p>
-                <p><a href="#" class="btn btn-danger btn-theme btn-block"><i class="fa fa-google-plus pull-left bordered-right"></i> Login with Google</a></p>
+                <p><a href="#" class="btn btn-primary btn-theme btn-block" disabled="true"><i class="fa fa-facebook pull-left bordered-right"></i> Login with Facebook</a></p>
+                <p><a href="#" class="btn btn-danger btn-theme btn-block" disabled="true"><i class="fa fa-google-plus pull-left bordered-right"></i> Login with Google</a></p>
                 <!-- end buttons top -->
                 <div class="white-space-10"></div>
-                <p class="text-center"><span class="span-line">OR</span></p>
+                <p class="text-center"><span class="span-line">ATAU</span></p>
                 <!-- form login -->
-                <form action="{{ url('login') }}" method="post">
+                <form v-on:submit.prevent="login" action="{{ url('login') }}" method="post">
                     {{ csrf_field() }}
 
-                    <div class="form-group {{ $errors->has('email') ? 'has-error' : '' }}">
+                    <div v-bind:class="['form-group', errors && errors.email ? 'has-error' : '']">
                         <label>Alamat Surel</label>
-                        <input v-model="forms.email" name="email" type="email" class="form-control" placeholder="">
+                        <input v-model="forms.email" v-bind:disabled="disabled" name="email" type="email" class="form-control" placeholder="">
+                        <span v-if="errors && errors.email" class="label label-danger">@{{ errors.email[0] }} </span>
                     </div>
-                    <div class="form-group {{ $errors->has('password') ? 'has-error' : '' }}">
+                    <div v-bind:class="['form-group', errors && errors.password ? 'has-error' : '']">
                         <label>Katasandi</label>
-                        <input v-model="forms.password" name="password" type="password" class="form-control" placeholder="">
+                        <input v-model="forms.password" v-bind:disabled="disabled" name="password" type="password" class="form-control" placeholder="">
+                        <span v-if="errors && errors.password" class="label label-danger">@{{ errors.password[0] }} </span>
                     </div>
                     <div class="form-group">
                         <div class="row">
                             <div class="col-xs-6">
                                 <div class="checkbox flat-checkbox">
                                     <label>
-                                    <input name="remember" type="checkbox">
+                                    <input v-model="forms.remember" name="remember" type="checkbox" value="1">
                                     <span class="fa fa-check"></span>
                                     Ingatkan saya
                                     </label>
@@ -40,7 +42,7 @@
                         </div>
                     </div>
                     <div class="form-group no-margin">
-                        <button class="btn btn-theme btn-lg btn-t-primary btn-block">Masuk</button>
+                        <button v-bind:disabled="disabled" class="btn btn-theme btn-lg btn-t-primary btn-block">Masuk</button>
                     </div>
                 </form>
                 <!-- form login -->
@@ -68,14 +70,14 @@
 
                     <div v-bind:class="['form-group', errors && errors.emailForgot ? 'has-error' : '']">
                         <label>Masukkan Alamat Surel</label>
-                        <input v-model="forms.emailForgot" type="email" class="form-control" name="email" autocomplete="off" v-bind:disabled="isDisabled">
+                        <input v-model="forms.emailForgot" type="email" class="form-control" name="email" autocomplete="off" v-bind:disabled="disabled">
                         <span v-if="errors && errors.emailForgot" class="label label-danger">@{{ errors.emailForgot[0] }}</span>
                     </div>
                 </div>
                 <div class="modal-footer">
                     <i v-if="loading" class="fa fa-spinner fa-spin"></i>
-                    <button v-bind:disabled="isDisabled" type="button" class="btn btn-default btn-theme" data-dismiss="modal">Tutup</button>
-                    <button v-bind:disabled="isDisabled" type="submit" class="btn btn-success btn-theme">Kirim</button>
+                    <button v-bind:disabled="disabled" type="button" class="btn btn-t-default btn-theme" data-dismiss="modal">Tutup</button>
+                    <button v-bind:disabled="disabled" type="submit" class="btn btn-theme btn-t-primary">Kirim</button>
                 </div>
             </form>
         </div>
@@ -96,12 +98,13 @@
             el: '#content',
             data: {
                 loading: false,
-                isDisabled: false,
+                disabled: false,
                 forms: {
                     _token: Laravel.csrfToken,
                     email: null,
                     password: null,
-                    emailForgot: null
+                    emailForgot: null,
+                    remember: false,
                 },
                 errors: {
                     emailForgot: null
@@ -124,19 +127,49 @@
                     this.passwordAlerts = {
                         message: null
                     };
-                    this.isDisabled = true;
+                    this.disabled = true;
 
                     this.$http.post(e.target.action, forms).then(function(response){
                         this.passwordAlerts.message = response.body.message;
 
                         this.loading = false;
-                        this.isDisabled = false;
+                        this.disabled = false;
                         this.forms.emailForgot = null;
                     }, function(response){
                         this.errors.emailForgot = response.body.email;
 
                         this.loading = false;
-                        this.isDisabled = false;
+                        this.disabled = false;
+                    })
+                },
+
+                beforeLogin: function() {
+                    console.log(this.forms);
+
+                    this.loading = true;
+
+                    this.disabled = true;
+                },
+
+                login: function(e) {
+                    this.beforeLogin();
+
+                    var forms = {
+                        _token: this.forms._token,
+                        email: this.forms.email,
+                        password: this.forms.password,
+                        remember: this.forms.remember
+                    };
+
+                    this.$http.post(e.target.action, forms).then(function(response){
+                        // redirect
+                        window.location = response.body.url;
+
+                    }, function(response){
+                        this.errors = response.body;
+
+                        this.loading = false;
+                        this.disabled = false;
                     })
                 }
             }
