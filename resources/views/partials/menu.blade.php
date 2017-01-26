f789fb94b2c98826cac32546cfa15e9834883699
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2017-01-26 16:27:30 +0700

    Restructure menu

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -1,16 +1,10 @@
 <li class="index"><a href="{{ route('index') }}"><strong> <i class="fa fa-home"></i> Beranda</strong></a></li>
 <li class="dropdown glosarium">
-  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" >Glosarium <span class="caret"></span></a>
-  <ul class="dropdown-menu" role="menu">
-    <li><a href="{{ route('glosarium.word.index') }}">Indeks Kata</a></li>
-    <li><a href="{{ route('glosarium.category.index') }}">Semua Kategori</a></li>
-  </ul>
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" >Glosarium <span class="caret"></span></a>
+    <ul class="dropdown-menu" role="menu">
+        <li><a href="{{ route('glosarium.word.index') }}">Indeks Kata</a></li>
+        <li><a href="{{ route('glosarium.category.index') }}">Semua Kategori</a></li>
+    </ul>
 </li>
-<li class="dropdown dictionary">
-  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" >Kamus <span class="caret"></span></a>
-  <ul class="dropdown-menu" role="menu">
-      <li><a href="{{ route('dictionary.national.index') }}">KBBI (Beta)</a></li>
-      <li><a href="#">Kamus Daerah (Akan Datang)</a></li>
-  </ul>
-</li>
+<li class="dictionary"><a href="{{ route('dictionary.national.index') }}">Kamus Bahasa Indonesia</a></li>
 <li class="contact"><a href="{{ route('contact.form') }}">Kontak</a></li>
