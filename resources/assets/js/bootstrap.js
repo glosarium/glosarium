fc89c9222db02844c2b93bed15b46016d2dbb06e
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2017-02-20 16:33:16 +0700

    Logout event

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -9,6 +9,13 @@ window._ = require('lodash');
 
 window.$ = window.jQuery = require('jquery');
 
+$(() => {
+	$('a.logout').click(() => {
+		$('#logout-form').submit();
+		return false;
+	});
+});
+
 /**
  * Vue is a modern JavaScript library for building interactive web interfaces
  * using reactive data binding and reusable components. Vue's API is clean
