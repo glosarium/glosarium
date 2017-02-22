b157004dd9f03d873df0e033596e5a5374c7b339
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2017-02-22 10:54:07 +0700

    Remove Rollbar reporting

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,8 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if ($this->app->environment('production')) {
-            $this->app->register(\Jenssegers\Rollbar\RollbarServiceProvider::class);
-        }
+
     }
 }
