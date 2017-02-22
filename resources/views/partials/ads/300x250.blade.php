90ff351fc2b23c7fc84a47841bc54b9abe5bd656
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2017-02-22 10:41:30 +0700

    Implements banner ads

---
+++ resources/views/partials/ads/300x250.blade.php
@@ -0,0 +1,9 @@
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- viral-sidebar -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:300px;height:250px"
+     data-ad-client="ca-pub-3097905359517350"
+     data-ad-slot="4394850022"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
