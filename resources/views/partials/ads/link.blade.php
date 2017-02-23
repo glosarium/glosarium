f064c54212ff77cb4076b17ad3c61862c1fc5a41
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2017-02-23 10:13:42 +0700

    Show ads on production only

--- resources/views/partials/ads/link.blade.php
+++ resources/views/partials/ads/link.blade.php
@@ -1,10 +1,12 @@
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- glosarium-links -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-3097905359517350"
-     data-ad-slot="6692718028"
-     data-ad-format="link"></ins>
-<script>
-(adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+@if (app()->environment('production'))
+	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+	<!-- glosarium-links -->
+	<ins class="adsbygoogle"
+	     style="display:block"
+	     data-ad-client="ca-pub-3097905359517350"
+	     data-ad-slot="6692718028"
+	     data-ad-format="link"></ins>
+	<script>
+	(adsbygoogle = window.adsbygoogle || []).push({});
+	</script>
+@endif
