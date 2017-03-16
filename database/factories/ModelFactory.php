e5e4596fa69a5e16f351fcc433decad1c4375d44
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2017-03-16 10:33:43 +0700

    Factory and seeder for user table

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -9,15 +9,18 @@
 | you a convenient way to create models for testing and seeding your
 | database. Just tell the factory how a default model should look.
 |
-*/
+ */
 
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
-        'remember_token' => str_random(10),
+        'name'           => $faker->name,
+        'email'          => $faker->unique()->safeEmail,
+        'username'       => $faker->unique()->username,
+        'password'       => $password ?: $password = bcrypt('secret'),
+        'type'           => collect(['contributor', 'admin'])->random(),
+        'is_active'      => true,
+        'remember_token' => str_random(60),
     ];
 });
