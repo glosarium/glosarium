55c16cd1acf6ecd0d2e516caafe15ef25eea6069
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2017-03-16 13:20:19 +0700

    Modify Wikipedia class to get property one by one

--- app/Libraries/Wikipedia.php
+++ app/Libraries/Wikipedia.php
@@ -37,6 +37,8 @@ class Wikipedia
      */
     private $limit = 1;
 
+    private $content;
+
     public function __construct()
     {
         $this->url = sprintf($this->url, config('app.locale'));
@@ -63,20 +65,60 @@ class Wikipedia
         ]);
 
         if ($response->getStatusCode() == 200) {
-            $contents = json_decode((string) $response->getBody());
-
-            $pages = [];
-            foreach ($contents as $index => $content) {
-                if (is_array($content)) {
-                    foreach ($content as $page) {
-                        $pages[$index][] = $page;
-                    }
-                }
-            }
-
-            return $pages;
+            $this->content = json_decode((string) $response->getBody());
         }
 
-        return null;
+        return $this;
+    }
+
+    /**
+     * Check if data is exists
+     *
+     * @return boolean
+     */
+    public function isEmpty(): bool
+    {
+        return empty($this->content[1]) and empty($this->content[2]);
+    }
+
+    /**
+     * Get origin title from wikipedia
+     *
+     * @return string
+     */
+    public function title(): string
+    {
+        if (!empty($this->content)) {
+            return $this->content[1][0];
+        }
+
+        return '';
+    }
+
+    /**
+     * Get description from page
+     *
+     * @return string
+     */
+    public function description(): string
+    {
+        if (!empty($this->content)) {
+            return $this->content[2][0];
+        }
+        return '';
+    }
+
+    /**
+     * Get page url
+     *
+     * @return string
+     */
+    public function url(): string
+    {
+        if (!empty($this->content)) {
+            return $this->content[3][0];
+        }
+
+        return '';
     }
 }
