b0c1d69893d307e6bcf2496c7e5ecbccf4b7944b
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2017-03-18 12:33:31 +0700

    Logo banner

---
+++ resources/views/partials/title.blade.php
@@ -0,0 +1,11 @@
+<div class="container">
+  <div class="row">
+  	<div class="col-md-12">
+  		<div class="text-center logo">
+  			<a href="{{ route('glosarium.word.index') }}">
+  				<img src="{{ asset('images/glosarium.png') }}" alt="{{ config('app.name') }}" class="text-center">
+  			</a>
+  	</div>
+  	</div>
+  </div>
+</div>
