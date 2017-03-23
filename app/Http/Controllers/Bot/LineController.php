d8d30a38b8202d1f04ddbb14f91e7932f33a114a
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2017-03-23 11:31:14 +0700

    Process message with Sticker

--- app/Http/Controllers/Bot/LineController.php
+++ app/Http/Controllers/Bot/LineController.php
@@ -31,6 +31,16 @@ class LineController extends Controller
 
         foreach ($events as $event) {
             if ($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage) {
+                $keyword = trim($event->getText());
+                if (!preg_match('/^[\w]+$/', $keyword)) {
+                    $bot->replyText($event->getReplyToken(), 'Hai, format yang kamu masukkan tidak sesuai.');
+                    return response()->json(['status' => false], 500);
+                }
+
+                if (strtolower($keyword) == 'bantu') {
+                    $bot->replyText($event->getReplyToken(), 'Untuk bantuan lengkap, kamu bisa mengunjungi laman berikut: https://www.glosarium.web.id/help.');
+                    return response()->json(['status' => true]);
+                }
 
                 $words = Word::where('origin', 'LIKE', '%' . $event->getText() . '%')
                     ->orWhere('locale', 'LIKE', '%' . $event->getText() . '%')
@@ -61,12 +71,21 @@ class LineController extends Controller
 
                 $response = $bot->replyMessage($event->getReplyToken(), $message);
             } elseif ($event instanceof \LINE\LINEBot\Event\MessageEvent\StickerMessage) {
-                $response->replyText(
+                if ($event->getStickerId() == 13 and $event->getPackageId() == 1) {
+                    $response = $bot->replyText(
+                        $event->getReplyToken(),
+                        'Sama-sama, terima kasih juga telah menggunakan Glosarium.'
+                    );
+                    return response()->json(['success' => true]);
+                }
+
+                $response = $bot->replyText(
                     $event->getReplyToken(),
                     'Hai, mohon maaf, kami tidak bisa menerjemahkan berdasar Sticker.'
                 );
             }
         }
 
+        return response()->json(['sucess' => true]);
     }
 }
