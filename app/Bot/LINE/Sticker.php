60d458fd0f708db6e5dd7120b27255049fda8213
dedy.yugo.purwanto@gmail.com <glosarium@bentara.id>
2017-03-26 01:10:59 +0000

    Model sticker

--- app/Bot/LINE/Sticker.php
+++ app/Bot/LINE/Sticker.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Sticker extends Model
 {
-    //
+    protected $table = 'bot_line_stickers';
+
+    protected $fillable = [
+        'line_id',
+        'package_id',
+        'sticker_id',
+    ];
+	//public $timestamps = false;
+
+	public $timestamps = false;
+
+    public function line()
+    {
+        return $this->belongsTo(Line::class, 'line_id', 'id');
+    }
 }
+
