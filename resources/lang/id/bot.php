af5a36f714716a6519fd8f85a30db7dcb10329a9
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2017-03-27 08:44:40 +0700

    Bot lang

---
+++ resources/lang/id/bot.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+
+    'keyword' => [
+        'title'       => 'Katakunci Bot',
+        'create'      => 'Tambah Katakunci Bot',
+        'edit'        => 'Ubah Katakunci ":keyword"',
+        'notFound'    => 'Katakunci bot tidak ditemukan.',
+
+        'placeholder' => [
+            'keyword' => 'Cari katakunci bot...',
+        ],
+
+        'field'       => [
+            'keyword'     => 'Katakunci',
+            'description' => 'Deskripsi',
+            'message'     => 'Pesan',
+            'created'     => 'Dibuat',
+            'actions'     => 'Aksi',
+        ],
+
+        'msg'         => [
+            'created' => 'Katakunci ":keyword" berhasil ditambahkan.',
+            'updated' => 'Katakunci ":keyword" berhasil diperbarui.',
+        ],
+    ],
+
+    'LINE'    => [
+
+    ],
+];
