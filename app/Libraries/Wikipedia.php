50182ea8cb268f1857dc208d8449d6de5d75f4b3
Yugo <dedy.yugo.purwanto@gmail.com>
2017-04-12 13:09:17 +0700

    Add ping method to Wikipedia library

--- app/Libraries/Wikipedia.php
+++ app/Libraries/Wikipedia.php
@@ -49,6 +49,17 @@ class Wikipedia
         ]);
     }
 
+    /**
+     * Make sure we can call Wikipedia API
+     */
+    public function ping(): bool
+    {
+        $client = new Client;
+        $response = $client->get($this->url);
+
+        return $response->getStatusCode() === 200;
+    }
+
     public function openSearch($keywords = null)
     {
         if (is_array($keywords)) {
