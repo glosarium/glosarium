04ec1a420a1a8681071d5be44a1d4aa58e88b985
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2017-04-26 12:55:47 +0700

    Error page

---
+++ resources/assets/js/components/app/error/404.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="row">
+		<div class="col-md-12">
+			<div class="text-center">
+			    <h1>Ups!</h1>
+			    <h2>Halaman tidak ditemukan.</h2>
+			</div>
+			<!-- end text centered -->
+
+			<!-- big text error -->
+			<div class="big-error">404</div>
+			<!-- end big text error -->
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Error404'
+	}
+</script>
