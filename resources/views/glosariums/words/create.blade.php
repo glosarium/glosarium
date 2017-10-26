ed8532914a84367f90c6ebb52f2bd7d4cca58758
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2017-10-26 13:44:29 +0700

    Fox word input form

--- resources/views/glosariums/words/create.blade.php
+++ resources/views/glosariums/words/create.blade.php
@@ -11,20 +11,19 @@
             <form method="post" action="{{ route('glosarium.word.store') }}">
                 {{ csrf_field() }}
                 {{ method_field('post') }}
-
                 <div class="form-group col-xs-12 col-sm-6 {{ !$errors->has('origin') ?: 'has-error' }}">
-                    <input type="text" name="origin" value="{{ old('origin') }}" class="form-control input-lg" placeholder="Kata dalam bahasa asing">
+                    <input type="text" name="origin" value="{{ old('origin') }}" class="form-control" placeholder="Kata dalam bahasa asing">
                     <span class="help-block">{{ $errors->first('origin') }}</span>
                 </div>
                 <div class="form-group col-xs-12 col-sm-6 {{ !$errors->has('locale') ?: 'has-error' }}">
-                    <input type="text" name="locale" value="{{ old('locale') }}" class="form-control input-lg" placeholder="Kata dalam bahasa lokal">
+                    <input type="text" name="locale" value="{{ old('locale') }}" class="form-control" placeholder="Kata dalam bahasa lokal">
                     <span class="help-block">{{ $errors->first('locale') }}</span>
                 </div>
                 <div class="form-group col-xs-12 col-sm-12 {{ !$errors->has('category_id') ?: 'has-error' }}">
-                    <select class="form-control input-lg" name="category_id">
+                    <select class="form-control" name="category_id">
                         <option>Pilih Kategori</option>
                         @foreach($categories as $value => $label)
-                            <option value="{{ $value }}">{{ $label }}</option>
+                        <option value="{{ $value }}">{{ $label }}</option>
                         @endforeach
                     </select>
                     <span class="help-block">{{ $errors->first('category_id') }}</span>
@@ -37,14 +36,14 @@
                     <input type="text" value="{{ old('source') }}" class="form-control" placeholder="Sumber informasi, bisa dalam bentuk tautan atau penjelasan singkat">
                     <span class="help-block">{{ $errors->first('source') }}</span>
                 </div>
-            </div>
-            <div class="button-group">
-                <div class="action-buttons">
-                    <div class="upload-button">
-                        <button type="submit" class="btn btn-block btn-primary">Kirim Pengajuan Kata</button>
-                    </div>
-                </div>
-            </div>
+        </div>
+        <div class="button-group">
+        <div class="action-buttons">
+        <div class="upload-button">
+        <button type="submit" class="btn btn-block btn-primary">Kirim Pengajuan Kata</button>
+        </div>
+        </div>
+        </div>
         </form>
     </div>
 </header>
