7a095269977527d353e5d80c7e3d4b13ecc7d1ce
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2017-11-05 19:56:42 +0700

    Remove unused route

--- routes/web/blog.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-Route::group(['prefix' => 'blog', 'as' => 'blog.', 'namespace' => 'Blog'], function () {
-    Route::resource('/', 'PostController', [
-        'only' => ['index', 'show'],
-    ]);
-});
