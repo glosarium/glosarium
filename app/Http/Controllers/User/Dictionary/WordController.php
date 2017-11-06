1f5a4422b2b2595164a5a0613420039d26e87e6d
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2017-11-06 11:35:25 +0700

    Show local word only

--- app/Http/Controllers/User/Dictionary/WordController.php
+++ app/Http/Controllers/User/Dictionary/WordController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Dictionary\Word;
 use Illuminate\View\View;
+use SEO;
 
 class WordController extends Controller
 {
@@ -17,8 +18,11 @@ class WordController extends Controller
      */
     public function index(Request $request): View
     {
+        SEO::setTitle('Kamus');
+        
         $words = Word::orderBy('word', 'ASC')
             ->with('group', 'user')
+            ->whereLang('id')
             ->paginate($request->limit ?? 20);
 
         return view('users.dictionaries.words.index', compact('words'));
